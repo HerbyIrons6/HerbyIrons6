8071e8e2a79d9c8b0ada6cbc129ca9440fe2cd0b
herby irons <97166914+HerbyIrons6@users.noreply.github.com>
2022-01-05 15:53:02 +0200

    Rostislav

---
+++ herby.py
@@ -0,0 +1,17 @@
+print ('We all live in a yellow submarine...')
+name = 'all live in a yellow submarine...'
+print(f'We {name}')
+first_name = 'Rostik'
+surname = ' Mykhailenko'
+print(f'{first_name + surname }')
+print('{:_>9}'.format('space'))
+print('{:_<9}'.format('space'))
+print('{:_^13}'.format('space') )
+
+x = 5
+y = 5
+
+print(f'Dодавання {x} + {y} = {x + y}')
+print(f'Віднімання {x} - {y} = {x - y}')
+print(f'Множення {x} * {y} = {x * y}')
+print(f'Ділення {x} / {y} = {x / y}')
