12a64eee8ab6484c098cf1442b981ec80469e500
herby irons <97166914+HerbyIrons6@users.noreply.github.com>
2022-01-05 16:55:33 +0200

    Update herby.py

--- herby.py
+++ herby.py
@@ -3,10 +3,16 @@ name = 'all live in a yellow submarine...'
 print(f'We {name}')
 first_name = 'Rostik'
 surname = ' Mykhailenko'
-print(f'{first_name + surname }')
+print('{first_name + surname }')
 print('{:_>9}'.format('space'))
 print('{:_<9}'.format('space'))
 print('{:_^13}'.format('space') )
+
+
+welcome = "Hellow"
+print(welcome)
+number_string = 5236
+print(number_string)
 
 x = 5
 y = 5
@@ -15,3 +21,24 @@ print(f'Dодавання {x} + {y} = {x + y}')
 print(f'Віднімання {x} - {y} = {x - y}')
 print(f'Множення {x} * {y} = {x * y}')
 print(f'Ділення {x} / {y} = {x / y}')
+print(f'Степінь {x} ** {y} = {x ** y}')
+print(f'Цілочисльне {x} // {y} = {x // y}')
+
+triple_quotes = '''
+
+
+#########
+#       #
+#       #
+#       #
+#########
+
+#       #
+#       #
+#########
+#       #
+#       #
+
+    '''
+print(triple_quotes)
+
