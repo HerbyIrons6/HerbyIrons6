868dfe3761f76b7a96b233fdf92cfa8a8449c206
herby irons <97166914+HerbyIrons6@users.noreply.github.com>
2022-01-17 15:47:25 +0200

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,27 @@
+
+
+
+print("Welcome to my calculator!")
+
+while True:
+    s = input("Please select the action you want to apply: +,-,*,/: ")
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        x = float(input(""))
+        y = float(input(""))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("Division by zero!")
+    else:
+        print("Error!")
+
+print("Thanks for using.  ")
